3472802a22acaf052002ecb93716fa97c648c04d
tj1805 <40129311+tj1805@users.noreply.github.com>
2021-01-20 16:12:37 +0100

    Initial commit

---
+++ HomeCraft.Data/HomeCraftDbContext.cs
@@ -0,0 +1,17 @@
+﻿using HomeCraft.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeCraft.Data
+{
+    public class HomeCraftDbContext : DbContext
+    {
+        public HomeCraftDbContext(DbContextOptions<HomeCraftDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
