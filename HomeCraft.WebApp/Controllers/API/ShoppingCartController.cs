38deee395968c991eb7b9f293119cba1d6e598db
tj1805 <40129311+tj1805@users.noreply.github.com>
2021-01-31 00:07:10 +0100

    more work on shopping cart feature

--- HomeCraft.WebApp/Controllers/API/ShoppingCartController.cs
+++ HomeCraft.WebApp/Controllers/API/ShoppingCartController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeCraft.Data.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,17 @@ namespace HomeCraft.WebApp.Controllers.API
 {
     public class ShoppingCartController
     {
+        private readonly IProductsRepository _productsRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IProductsRepository productsRepository, ShoppingCart shoppingCart)
+        {
+            _productsRepository = productsRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        
+
     }
 }
+ 
